0966f4b892655fe1d063a3ce3bb9bdb6fa1dd8e7
Romain LEMOINE <rlemoine.externe@entoria.fr>
2021-08-06 11:18:55 +0200

    Use of jquery DataTables for log lists

--- src/Resources/public/js/logviewer.js
+++ src/Resources/public/js/logviewer.js
@@ -1,17 +1,37 @@
-const myTable = document.querySelector("#logTable");
-const logsUrl = myTable.getAttribute('data-remote-url');
-const dataTable = new simpleDatatables.DataTable(myTable);
-
-var xhttp = new XMLHttpRequest();
-xhttp.dataTable = dataTable;
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        this.dataTable.import({
-            type: "json",
-            data: this.responseText
-        });
-    }
-};
-
-xhttp.open("GET", logsUrl, true);
-xhttp.send();
+$(document).ready( function () {
+    $('#logsTable').DataTable({
+        "stateSave": true,
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            url: $('#logsTable').data('remote-url'),
+            dataType: "json",
+            data: function ( d ) {
+                d.var1 = 'value';
+            },
+            error: function (xhr, error, thrown) {
+                $("#dt-overlay").hide();
+            }
+        },
+        "preDrawCallback": function( settings ) {
+            $("#dt-overlay").show();
+        },
+        "initComplete" : function(settings, json) {
+            $("#dt-overlay").hide();
+        },
+        "order": [[ 0, "desc" ]],
+        "columnDefs": [
+            {
+                "targets": "no-sort",
+                "orderable": false,
+            }
+        ],
+        "columns": [
+            {"data": "date"},
+            {"data": "context"},
+            {"data": "level"},
+            {"data": "description"},
+            {"data": "body"}
+        ]
+    });
+});
